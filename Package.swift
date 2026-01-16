7c62fe2714f5ab4bcc7f8fe825823e3d10f45563
Matvii Sakhnenko <matvey.sakhnenko03@icloud.com>
2026-01-16 17:06:53 +0100

    [Clean] Rename package to AppCorePackages

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "CleanTemplatePackages",
+    name: "AppCorePackages",
     platforms: [.iOS(.v18)],
     products: [
         .library(
